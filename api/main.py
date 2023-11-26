62a033ec12f5b9984854b1e0dae31367ff42e46a
i-rud <ivannrudd@gmail.com>
2023-11-26 12:42:51 +0100

    Enable CORS

--- api/main.py
+++ api/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from nba_processor import NBAProcessor
 
 app = FastAPI()
+
+origins = [
+    # Front-End App
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/nba/live_scoreboard")
